c0685ca55bccb5edc974c10be8ab6aa7681fd6ab
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-10-30 01:48:58 +0600

    first commit

---
+++ src/app/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header-container">
+        <h2>
+          Crafting Digital Experiences with Precision and Innovation | Elevate
+          Your Web Presence with Expert Development
+        </h2>
+        <h5>
+          Architecting Responsive and Intuitive Websites for an Enhanced User
+          Journey. Your Vision, Our Code – Crafting Digital Excellence.
+        </h5>
+
+        <button>My Resume</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
