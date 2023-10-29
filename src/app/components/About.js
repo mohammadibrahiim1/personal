0c4e611af7fc504e6e877b8dea5576e1e563219a
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-10-30 03:01:08 +0600

    updated

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -8,17 +8,17 @@ const About = () => {
         <div className="hero-content flex-col lg:flex-row-reverse">
           <Image
             src="https://i.ibb.co/8Pst2r2/profile-img.jpg"
-            width={500}
-            height={500}
-            className="max-w-sm rounded-lg shadow-2xl"
+            width={360}
+            height={360}
+            className="max-w-sm rounded-lg shadow-2xl mx-auto"
             alt="Picture of the author"
           />
           <div>
             <h5>Hello,My name is</h5>
             <h1 className="text-5xl font-bold"> Mohammad Ebrahim</h1>
             <h3 className="text-lg font-bold pb-4">
-              MERN maestro weaving web wonders with MongoDB, Express, React, and
-              Node.js.
+              MERN maestro weaving web wonders with MongoDB, Express, React,{" "}
+              <br /> and Node.js.
             </h3>
 
             <button className="btn">Check Out My Resume</button>
